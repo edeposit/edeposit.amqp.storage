c7a9e61f6d624f5f7e86a64cc9e9d56eae4952a7
Bystroushaak <bystrousak@kitakitsune.org>
2015-06-04 12:00:55 +0200

    #4: Added AMQP connector reactToAMQPMessage().

--- src/edeposit/amqp/storage/__init__.py
+++ src/edeposit/amqp/storage/__init__.py
@@ -1,1 +1,50 @@
-__import__('pkg_resources').declare_namespace(__name__)
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Interpreter version: python 2.7
+#
+# Imports =====================================================================
+import structures
+
+from storage_handler import save_publication
+from storage_handler import search_publications
+
+
+# Functions & classes =========================================================
+def _instanceof(instance, cls):
+    """
+    Check type of `instance` by matching ``.__name__`` with `cls.__name__`.
+    """
+    return type(instance).__name__ == cls.__name__
+
+
+# Main function ===============================================================
+def reactToAMQPMessage(message, send_back):
+    """
+    React to given (AMQP) message. `message` is expected to be
+    :py:func:`collections.namedtuple` structure from :mod:`.structures` filled
+    with all necessary data.
+
+    Args:
+        message (object): One of the request objects defined in
+                          :mod:`.structures`.
+        send_back (fn reference): Reference to function for responding. This is
+                  useful for progress monitoring for example. Function takes
+                  one parameter, which may be response structure/namedtuple, or
+                  string or whatever would be normally returned.
+
+    Returns:
+        object: Response class from :mod:`structures`.
+
+    Raises:
+        ValueError: if bad type of `message` structure is given.
+    """
+    if _instanceof(message, structures.SaveRequest):
+        return save_publication(message.pub)
+
+    elif _instanceof(message, structures.SearchRequest):
+        return structures.SearchResult(
+            publications=search_publications(message.query)
+        )
+
+    raise ValueError("'%s' is unknown type of request!" % str(type(message)))
