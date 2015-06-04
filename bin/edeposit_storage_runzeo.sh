f2a7ebc7838db6f084b74c67a06b315b795f4937
Bystroushaak <bystrousak@kitakitsune.org>
2015-06-04 15:28:50 +0200

    Added content of the ZEO connector edeposit_storage_runzeo.sh.

--- bin/edeposit_storage_runzeo.sh
+++ bin/edeposit_storage_runzeo.sh
@@ -1,2 +1,7 @@
-#! /usr/bin/env sh
+#! /usr/bin/env bash
 
+SCRIPT_DIR=`dirname $0`
+cd "$SCRIPT_DIR/../src/edeposit/amqp/storage"
+cd `python -c "import settings; print settings.ZCONF_PATH"`
+
+runzeo -C zeo.conf
