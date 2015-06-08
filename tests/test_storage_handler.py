39023ae9c97e441491e22d5c715f6c2b7e797c45
Bystroushaak <bystrousak@kitakitsune.org>
2015-06-08 18:26:15 +0200

    #8: Fixed the ZEO runner for tests.

--- tests/test_storage_handler.py
+++ tests/test_storage_handler.py
@@ -5,18 +5,23 @@
 #
 # Imports =====================================================================
 import os
-import os.path
 import shutil
+import os.path
 import tempfile
+import subprocess
 from string import Template
+from multiprocessing import Process
 
 import pytest
 
+from storage import zconf
 from storage import settings
+from storage import storage_handler
 
 
 # Variables ===================================================================
 TMP_DIR = None
+SERV = None
 
 
 # Fixtures ====================================================================
@@ -26,32 +31,44 @@ def data_context(fn):
     with open(os.path.join(path, fn)) as f:
         return f.read()
 
-# @pytest.fixture
-# def fixture():
-#     pass
-
-# with pytest.raises(Exception):
-#     raise Exception()
-
 
 # Tests =======================================================================
 def setup_module(module):
     global TMP_DIR
     TMP_DIR = tempfile.mkdtemp()
 
-    with open(os.path.join(TMP_DIR, "zeo.conf"), "w") as f:
+    # monkey patch the paths
+    settings.ZCONF_PATH = TMP_DIR
+    zconf.ZCONF_PATH = TMP_DIR
+
+    # write ZEO server config to  temp directory
+    zeo_conf_path = os.path.join(TMP_DIR, "zeo.conf")
+    with open(zeo_conf_path, "w") as f:
         f.write(
             Template(data_context("zeo.conf")).substitute(
                 path=TMP_DIR
             )
         )
 
-    with open(os.path.join(TMP_DIR, "zeo_client.conf"), "w") as f:
+    # write client config to temp directory
+    client_config_path = os.path.join(TMP_DIR, "zeo_client.conf")
+    with open(client_config_path, "w") as f:
         f.write(data_context("zeo_client.conf"))
+
+    # run the ZEO server
+    def run_zeo():
+        subprocess.check_call("runzeo -C " + zeo_conf_path, shell=True)
+
+    global SERV
+    SERV = Process(target=run_zeo)
+    SERV.start()
 
 
 def test_get_db_connectors():
-    pass
+    print settings.ZCONF_PATH
+    connectors = storage_handler._get_db_connectors()
+
+    assert len(list(connectors)) > 1
 
 
 def test_check_pub_type():
@@ -60,3 +77,4 @@ def test_check_pub_type():
 
 def teardown_module(module):
     shutil.rmtree(TMP_DIR)
+    SERV.terminate()
