73106a22178f1fdd282389a166be69e5dda9e3c5
Bystroushaak <bystrousak@kitakitsune.org>
2015-06-09 19:03:58 +0200

    Fixed "Attempt to store an object from a foreign database connection" bug.

--- src/edeposit/amqp/storage/zconf/__init__.py
+++ src/edeposit/amqp/storage/zconf/__init__.py
@@ -7,12 +7,15 @@
 import os.path
 
 import ZODB.config
-import transaction
 from ZODB import DB
 from BTrees.OOBTree import OOBTree
 
 from ..settings import ZCONF_PATH
 from ..settings import PROJECT_KEY
+
+
+# Variables ===================================================================
+CONNECTION = None
 
 
 # Functions & classes =========================================================
@@ -25,11 +28,13 @@ def get_zeo_connection():
 
 
 def get_zeo_root():
-    conn = get_zeo_connection()
+    global CONNECTION
+    conn = CONNECTION if CONNECTION else get_zeo_connection()
+    CONNECTION = conn
+
     dbroot = conn.root()
 
     if PROJECT_KEY not in dbroot:
-        from BTrees.OOBTree import OOBTree
         dbroot[PROJECT_KEY] = OOBTree()
 
     return dbroot[PROJECT_KEY]
@@ -40,6 +45,5 @@ def get_zeo_key(key, new_obj=OOBTree):
 
     if not root.get(key, None):
         root[key] = new_obj()
-        transaction.commit()
 
     return root[key]
