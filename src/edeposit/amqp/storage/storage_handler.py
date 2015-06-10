51ecab4aaef7cc15e357d14b776d97d597e1bd46
Bystroushaak <bystrousak@kitakitsune.org>
2015-06-10 17:42:21 +0200

    #10: Added docstrings for all functions in storage_handler.py.

--- src/edeposit/amqp/storage/storage_handler.py
+++ src/edeposit/amqp/storage/storage_handler.py
@@ -7,7 +7,6 @@
 import transaction
 from BTrees.OOBTree import OOTreeSet, intersection
 
-
 from zconf import get_zeo_key
 
 from structures import DBPublication
@@ -30,14 +29,31 @@ class UnindexablePublication(Exception):
 
 # Functions & classes =========================================================
 def _get_db_connectors():
+    """
+    Return list of database dictionaries, which are used as indexes for each
+    attributes.
+
+    Returns:
+        list: List of OOBTree's for each item in :attr:`.COMMON_FIELDS`.
+    """
     for field_name, docstring in COMMON_FIELDS:
-        if "(bool" in docstring:
+        if "(bool" in docstring:  # TODO: try to remove this
             continue
 
         yield field_name, get_zeo_key(field_name)
 
 
 def _check_pub_type(pub, name="pub"):
+    """
+    Make sure, that `pub` is instance of the :class:`.DBPublication`.
+
+    Args:
+        pub (obj): Instance which will be checked.
+        name (str): Name of the instance. Used in exception.
+
+    Raises:
+        InvalidType: When the `pub` is not instance of :class:`.DBPublication`.
+    """
     if not isinstance(pub, DBPublication):
         raise InvalidType(
             "`%s` to be instance of DBPublication, not %s!" % (
@@ -47,7 +63,17 @@ def _check_pub_type(pub, name="pub"):
         )
 
 
-def _index_publication(pub):
+def _put_into_indexes(pub):
+    """
+    Put publication into all indexes.
+
+    Attr:
+        pub (obj): Instance of :class:`.DBPublication`.
+
+    Raises:
+        UnindexablePublication: When there is no index (property) which can be
+                                used to index `pub` in database.
+    """
     no_of_used_indexes = 0
     for field_name, db_connector in list(_get_db_connectors()):
         attr = getattr(pub, field_name)
@@ -69,11 +95,22 @@ def _index_publication(pub):
 
 
 def save_publication(pub):
+    """
+    Save `pub` into database and into proper indexes.
+
+    Attr:
+        pub (obj): Instance of the :class:`.DBPublication`.
+
+    Raises:
+        InvalidType: When the `pub` is not instance of :class:`.DBPublication`.
+        UnindexablePublication: When there is no index (property) which can be
+                                used to index `pub` in database.
+    """
     _check_pub_type(pub)
 
     with transaction.manager:
         # put publication into all indexes
-        _index_publication(pub)
+        _put_into_indexes(pub)
 
         # save to list of public/private publications
         list_db = get_zeo_key(
@@ -84,10 +121,19 @@ def save_publication(pub):
 
 
 def _get_subset_matches(query):
+    """
+    Yield publications, at indexes defined by `query` property values.
+
+    Args:
+        query (obj): :class:`.DBPublication` with `some` of the properties set.
+
+    Yields:
+        list: List of matching publications.
+    """
     for field_name, db_connector in _get_db_connectors():
         attr = getattr(query, field_name)
 
-        if not attr:
+        if attr is None:  # don't use unset attributes
             continue
 
         results = db_connector.get(attr, [])
@@ -97,6 +143,26 @@ def _get_subset_matches(query):
 
 
 def search_publications(query):
+    """
+    Return list of :class:`.DBPublication` which match all properties that are
+    set (``not None``) using AND operator to all of them.
+
+    Example:
+        result = storage_handler.search_publications(
+            DBPublication(isbn="azgabash")
+        )
+
+    Args:
+        query (obj): :class:`.DBPublication` with `some` of the properties set.
+
+    Returns:
+        list: List of matching :class:`.DBPublication` or ``[]`` if no match \
+              was found.
+
+    Raises:
+        InvalidType: When the `query` is not instance of \
+                     :class:`.DBPublication`.
+    """
     _check_pub_type(query, "query")
 
     # AND operator between results
