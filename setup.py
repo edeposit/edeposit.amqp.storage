66c1b9776420bdb3cf768719a023a648ae1e9f04
Bystroushaak <bystrousak@kitakitsune.org>
2015-06-12 15:07:15 +0200

    #14: Added setup.py.

---
+++ setup.py
@@ -0,0 +1,65 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Interpreter version: python 2.7
+#
+# Imports =====================================================================
+from setuptools import setup, find_packages
+from docs import getVersion
+
+
+# Variables ===================================================================
+changelog = open('CHANGELOG.rst').read()
+long_description = "\n\n".join([
+    open('README.rst').read(),
+    changelog
+])
+
+
+# Actual setup definition =====================================================
+setup(
+    name='edeposit.amqp.storage',
+    version=getVersion(changelog),
+    description="Binary storage for the Edeposit project.",
+    long_description=long_description,
+    url='https://github.com/edeposit/edeposit.amqp.storage/',
+
+    author='Edeposit team',
+    author_email='edeposit@email.cz',
+
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        'Intended Audience :: Developers',
+
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+
+        "License :: OSI Approved :: MIT License",
+    ],
+    license='MIT',
+
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['edeposit', 'edeposit.amqp'],
+
+    scripts=[
+        'bin/edeposit_storage_runzeo.sh',
+        'bin/edeposit_storage_server.py',
+    ],
+
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=open("requirements.txt").read().splitlines(),
+
+    test_suite='py.test',
+    tests_require=["pytest"],
+    extras_require={
+        "test": [
+            "pytest"
+        ],
+        "docs": [
+            "sphinx",
+            "sphinxcontrib-napoleon",
+        ]
+    },
+)
