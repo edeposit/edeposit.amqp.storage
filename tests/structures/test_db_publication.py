aabb57263b004adf5403a3b3ccad179f5f922263
Bystroushaak <bystrousak@kitakitsune.org>
2015-06-16 14:09:53 +0200

    #8: Added extended tests of the DBPublication.

--- tests/structures/test_db_publication.py
+++ tests/structures/test_db_publication.py
@@ -8,6 +8,9 @@ from __future__ import unicode_literals
 
 import uuid
 import random
+import copy
+
+from BTrees.OOBTree import OOTreeSet
 
 import pytest
 from faker import Factory
@@ -36,5 +39,54 @@ def random_publication():
 
 
 # Tests =======================================================================
-# def test_():
-#     pass
+def test_op_eq(random_publication):
+    rand_copy = copy.deepcopy(random_publication)
+
+    assert random_publication == rand_copy
+    # assert random_publication.__hash__() == rand_copy.__hash__()
+    assert not (random_publication != rand_copy)
+
+    rand_copy.title = "azgabash"
+
+    assert not (random_publication == rand_copy)  # op eq
+    # assert random_publication.__hash__() != rand_copy.__hash__()
+    assert random_publication != rand_copy
+
+
+def test_in_operator():
+    rp1 = random_publication()
+    rp2 = random_publication()
+    rp3 = random_publication()
+
+    assert rp1 != rp2
+
+    cont = set([rp1, rp2])
+
+    assert rp1 in cont
+    assert rp2 in cont
+
+    assert rp3 not in cont
+
+
+def test_OOTreeSet():
+    a = OOTreeSet()
+    rp1 = random_publication()
+    rp2 = random_publication()
+    rp3 = random_publication()
+
+    a.insert(rp1)
+
+    assert rp1 in a
+    assert rp2 not in a
+    assert rp3 not in a
+    assert not (rp2 in a)
+    assert not (rp3 in a)
+
+    a.insert(rp2)
+    assert len(a) == 2
+
+    assert rp1 in a
+    assert rp2 in a
+    assert not (rp2 not in a)
+    assert rp3 not in a
+    assert not (rp3 in a)
