7e6fa53d98c1497345340c9661bc1170e385dd47
Bystroushaak <bystrousak@kitakitsune.org>
2015-08-25 14:58:08 +0200

    Added test of the AMQP chain. Closes #33.

--- tests/test_amqp_chain.py
+++ tests/test_amqp_chain.py
@@ -85,4 +85,35 @@ def test_publication_save(pdf_publication):
     assert result.publications[0].producent_id == pdf_publication.producent_id
     assert result.publications[0].is_public == pdf_publication.is_public
     assert result.publications[0].filename == pdf_publication.filename
+    assert len(result.publications[0].b64_data) > 100
+    assert "http" in result.publications[0].url
+
+
+def test_publication_light_request(pdf_publication):
+    storage.reactToAMQPMessage(
+        storage.SaveRequest(pdf_publication),
+        lambda x: x
+    )
+
+    result = storage.reactToAMQPMessage(
+        storage.SearchRequest(
+            storage.Publication(isbn=pdf_publication.isbn),
+            light_request=True
+        ),
+        lambda x: x
+    )
+
+    assert result.publications
+
+    assert result.publications[0].title == pdf_publication.title
+    assert result.publications[0].author == pdf_publication.author
+    assert result.publications[0].pub_year == pdf_publication.pub_year
+    assert result.publications[0].isbn == pdf_publication.isbn
+    assert result.publications[0].urnnbn == pdf_publication.urnnbn
+    assert result.publications[0].uuid == pdf_publication.uuid
+    assert result.publications[0].aleph_id == pdf_publication.aleph_id
+    assert result.publications[0].producent_id == pdf_publication.producent_id
+    assert result.publications[0].is_public == pdf_publication.is_public
+    assert result.publications[0].filename == pdf_publication.filename
+    assert not result.publications[0].b64_data
     assert "http" in result.publications[0].url
