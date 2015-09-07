0df3bead5c0dee00fe912a9eb74895c3c6842a2a
Bystroushaak <bystrousak@kitakitsune.org>
2015-09-07 15:15:21 +0200

    #35: Added new template generator.

---
+++ src/edeposit/amqp/storage/structures/templates/__init__.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Interpreter version: python 2.7
+#
+# Imports =====================================================================
+import os.path
+from bottle import template
+
+import shared
+
+
+# Functions ===================================================================
+def _apply_template(my_template):
+    return template(
+        my_template,
+        CLASS_NAME=shared.CLASS_NAME,
+        COMMUNICATION_FIELDS=shared.COMMUNICATION_FIELDS,
+        DATABASE_FIELDS=shared.DATABASE_FIELDS,
+        SAVEABLE_FILES=shared.SAVEABLE_FILES,
+    )
+
+
+def _read_file(fn):
+    fn = os.path.join(
+        os.path.dirname(__file__),
+        fn
+    )
+
+    with open(fn) as f:
+        return f.read()
+
+
+def get_db_publication():
+    return _apply_template(
+        _read_file("db_publication_template.py")
+    )
+
+
+def get_publication():
+    return _apply_template(
+        _read_file("publication_template.py")
+    )
