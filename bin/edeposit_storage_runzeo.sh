77f20fb1980ea345ee5d6abe38eade317d19d555
Bystroushaak <bystrousak@kitakitsune.org>
2015-10-13 12:05:57 +0200

    Fixed typos in comment.

--- bin/edeposit_storage_runzeo.sh
+++ bin/edeposit_storage_runzeo.sh
@@ -2,8 +2,7 @@
 
 cd `python -c "import edeposit.amqp.storage.settings as s; print s.ZCONF_PATH"`
 
-# supervisord can't will stop the script, but let the runzeo running, this
-# should fix it
+# supervisord can't stop the script, this should fix it
 trap "{ pkill runzeo -SIGINT; exit 0; }" EXIT
 
 runzeo -C zeo.conf
