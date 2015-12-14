5d929fe3c2f66ffa10d93dcc4702152f4ecdeaff
Bystroushaak <bystrousak@kitakitsune.org>
2015-12-14 17:41:29 +0100

    Test init file is no longer neccessary.

--- tests/__init__.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-# Interpreter version: python 2.7
-#
-# Imports =====================================================================
-
-# This file is here because of the relative imports at pacakge level from
-# structures/ tests.
