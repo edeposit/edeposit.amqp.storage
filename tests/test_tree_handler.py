c7ce62d873f858f2308a658376e5e8a356d6b018
Bystroushaak <bystrousak@kitakitsune.org>
2015-12-18 17:01:55 +0100

    #47: Added basic tests of TreeHandler.

---
+++ tests/test_tree_handler.py
@@ -0,0 +1,106 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Interpreter version: python 2.7
+#
+# Imports =====================================================================
+import pytest
+
+from storage.structures import Tree
+from storage.tree_handler import TreeHandler
+
+
+# Variables ===================================================================
+# Fixtures ====================================================================
+@pytest.fixture
+def sample_tree():
+    return Tree(
+        name="Mladá fronta",
+        sub_trees=[
+            Tree(
+                name="2015",
+                sub_trees=[],
+                sub_publications=[
+                    "first",
+                    "second"
+                ],
+                aleph_id=None,
+                issn=None,
+                path="Mladá fronta/2015",
+            ),
+            Tree(
+                name="2014",
+                sub_trees=[],
+                sub_publications=[
+                    "first",
+                    "second"
+                ],
+                aleph_id=None,
+                issn=None,
+                path="Mladá fronta/2014",
+            ),
+        ],
+        sub_publications=[
+        ],
+        aleph_id="0005389",
+        issn="1805-8787",
+        path="Mladá fronta",
+    )
+
+
+@pytest.fixture(scope="module", autouse=True)
+def tree_handler(client_conf_path):
+    return TreeHandler(conf_path=client_conf_path)
+
+
+# Tests =======================================================================
+def test_tree(sample_tree):
+    assert sample_tree
+
+
+def test_tree_handler_add_tree(sample_tree, tree_handler):
+    tree_handler.add_tree(sample_tree)
+
+    assert sample_tree in tree_handler.name_db[sample_tree.name]
+    assert sample_tree in tree_handler.aleph_id_db[sample_tree.aleph_id]
+    assert sample_tree in tree_handler.issn_db[sample_tree.issn]
+    assert sample_tree in tree_handler.path_db[sample_tree.path]
+    assert sample_tree.path not in tree_handler.parent_db
+
+    first_subtree = sample_tree.sub_trees[0]
+    second_subtree = sample_tree.sub_trees[1]
+
+    assert first_subtree in tree_handler.name_db[first_subtree.name]
+    # assert first_subtree.aleph_id not in tree_handler.aleph_id_db
+    # assert first_subtree.issn not in tree_handler.issn_db
+    assert first_subtree in tree_handler.path_db[first_subtree.path]
+    assert sample_tree in tree_handler.parent_db[first_subtree.path]
+
+    assert second_subtree in tree_handler.name_db[second_subtree.name]
+    # assert second_subtree.aleph_id not in tree_handler.aleph_id_db
+    # assert second_subtree.issn not in tree_handler.issn_db
+    assert second_subtree in tree_handler.path_db[second_subtree.path]
+    assert sample_tree in tree_handler.parent_db[second_subtree.path]
+
+
+def test_tree_handler_remove_tree(sample_tree, tree_handler):
+    tree_handler.remove_tree(sample_tree)
+
+    assert sample_tree.name not in tree_handler.name_db
+    assert sample_tree.aleph_id not in tree_handler.aleph_id_db
+    assert sample_tree.issn not in tree_handler.issn_db
+    assert sample_tree.path not in tree_handler.path_db
+    assert sample_tree.path not in tree_handler.parent_db
+
+    first_subtree = sample_tree.sub_trees[0]
+    second_subtree = sample_tree.sub_trees[1]
+
+    assert first_subtree.name not in tree_handler.name_db
+    # assert first_subtree.aleph_id not in tree_handler.aleph_id_db
+    # assert first_subtree.issn not in tree_handler.issn_db
+    assert first_subtree.path not in tree_handler.path_db
+
+    assert second_subtree.name not in tree_handler.name_db
+    # assert second_subtree.aleph_id not in tree_handler.aleph_id_db
+    # assert second_subtree.issn not in tree_handler.issn_db
+    assert second_subtree.path not in tree_handler.path_db
