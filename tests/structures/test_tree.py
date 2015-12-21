5f94aee016f49eefc9e0522206632945bc950074
Bystroushaak <bystrousak@kitakitsune.org>
2015-12-21 16:54:18 +0100

    #47: Added tests of tree automatic generation paths.

--- tests/structures/test_tree.py
+++ tests/structures/test_tree.py
@@ -4,7 +4,43 @@
 # Interpreter version: python 2.7
 #
 # Imports =====================================================================
+import pytest
+
 from storage.structures import Tree
+
+
+# Fixtures ====================================================================
+@pytest.fixture
+def sample_tree():
+    return Tree(
+        name="Mladá fronta",
+        sub_trees=[
+            Tree(
+                name="2015",
+                sub_trees=[],
+                sub_publications=[
+                    "first",
+                    "second"
+                ],
+                aleph_id=None,
+                issn=None,
+            ),
+            Tree(
+                name="2014",
+                sub_trees=[],
+                sub_publications=[
+                    "first",
+                    "second"
+                ],
+                aleph_id=None,
+                issn=None,
+            ),
+        ],
+        sub_publications=[
+        ],
+        aleph_id="0005389",
+        issn="1805-8787",
+    )
 
 
 # Tests =======================================================================
@@ -23,7 +59,7 @@ def test_Tree():
     assert t.sub_publications == ["first", "second"]
     assert t.aleph_id is None
     assert t.issn is None
-    assert t.path == ""
+    assert t.path == "2015"
 
 
 def test_Tree_set_path():
@@ -63,3 +99,9 @@ def test_Tree_set_path_array():
     assert t.aleph_id is None
     assert t.issn is None
     assert t.path == "Mladá fronta"
+
+
+def test_sample_tree(sample_tree):
+    assert sample_tree.path == "Mladá fronta"
+    assert sample_tree.sub_trees[0].path == "Mladá fronta/2015"
+    assert sample_tree.sub_trees[1].path == "Mladá fronta/2014"
