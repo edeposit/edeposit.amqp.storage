df3d9e855bb7bf0184314649212adff53e707a84
Bystroushaak <bystrousak@kitakitsune.org>
2015-12-22 14:37:01 +0100

    #47: Added new response TreeInfo.

--- src/edeposit/amqp/storage/structures/comm/responses.py
+++ src/edeposit/amqp/storage/structures/comm/responses.py
@@ -15,3 +15,14 @@ class SearchResult(namedtuple("SearchResult", ["records"])):
     Attributes:
         records (list): List of matching :class:`.Publication` objects.
     """
+
+
+class TreeInfo(namedtuple("TreeInfo", "path url_by_path url_by_issn")):
+    """
+    Informations about stored trees.
+
+    Attributes:
+        path (str): Path of the tree in storage.
+        url_by_path (str): Full url-encoded path of the tree in storage.
+        url_by_issn (str): URL composed from ISSN.
+    """
